7ed80b7bbec3c98b2c1f6e8348146a2e25bf091b
Iris Rattanasinh <gracie3607@gmail.com>
2025-11-12 10:56:54 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./Login";
+import Signup from "./Signup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav style={{ textAlign: "center", margin: "20px" }}>
+        <Link to="/login" style={{ margin: "10px" }}>Login</Link>
+        <Link to="/signup" style={{ margin: "10px" }}>Sign Up</Link>
+      </nav>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Login.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./auth.css";
+
+function Login() {
+  return (
+    <div className="auth-container">
+      <h2>Login</h2>
+      <form>
+        <input type="email" placeholder="Email" required />
+        <input type="password" placeholder="Password" required />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
